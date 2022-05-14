043ebe681e9240c409ecb5565dc3454015a16986
Edafe Oke <greatedafeoke@gmail.com>
2022-05-14 04:39:20 +0100

    Added mysql details

--- user/register.php
+++ user/register.php
@@ -335,7 +335,7 @@ if (isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login <?php echo $bank_name; ?></title>
+    <title>Register <?php echo $bank_name; ?></title>
     <!-- Favicons -->
     <link href="../assets/img/favicon-32x32.png" rel="icon">
     <link href="../assets/img/apple-icon-180x180.png" rel="apple-touch-icon">
