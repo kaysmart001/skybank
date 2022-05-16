cc811dac6e4b52565e0caa322f3a79f2784afd23
Edafe Oke <greatedafeoke@gmail.com>
2022-05-16 22:40:49 +0100

    Added files

--- bankConfig.php
+++ bankConfig.php
@@ -1,13 +1,13 @@
 <?php
 
-$bank_name = "National Bank plc";
-$bank_mail = 'greatedafeoke@gmail.com';
+$bank_name = "Sky Reliance";
+$bank_mail = 'admin@skyreliance.org';
 $bank_password = '';
 $bank_number = '+1 5589 55488 55';
 $bank_address = 'Sky Tower <br>
 New Mumbai, NY 535022<br>
 India <br><br>';
 
-$bank_url = 'www.nationalbankplc.com';
+$bank_url = 'www.skyreliance.org';
 $rate = 7; //Loan rate
 ?>
